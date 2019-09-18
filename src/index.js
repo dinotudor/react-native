67b1e7cd4831b9ba18f9e9537cfbc34308f3dc2a
Dino Tudor <dinotudor@gmail.com>
2019-09-18 19:09:16 +0200

    gesture handler, navigation, stack, create Route.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 32,
-    fontWeight: 'bold',
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>React Native Project</Text>
-    </View>
-  );
+  return <Routes />;
 }
