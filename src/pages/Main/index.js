17ef8a66a3bc7bec5e83e864d445cd629083a346
Dino Tudor <dinotudor@gmail.com>
2019-09-18 20:48:13 +0200

    react-native-vector-icons

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, Submit } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autocorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+        />
+        <Submit>
+          <Icon name="add" size={20} color="#FFF" />
+        </Submit>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
