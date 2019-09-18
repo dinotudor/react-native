0061ad29143b2e46429156e4f5c8480c6729cfdf
Dino Tudor <dinotudor@gmail.com>
2019-09-18 21:16:21 +0200

    eslint rule for state inside constructor, index to class component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Form, Input, Submit } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autocorrect={false}
-          autoCapitalize="none"
-          placeholder="Add user"
-        />
-        <Submit>
-          <Icon name="add" size={20} color="#FFF" />
-        </Submit>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = () => {
+    console.tron.log(this.state.newUser);
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autocorrect={false}
+            autoCapitalize="none"
+            placeholder="Add user"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+          />
+          <Submit onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </Submit>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
