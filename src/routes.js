1a558f2b1e42cf8d385b093ebe8581c57d5f92bc
Dino Tudor <dinotudor@gmail.com>
2019-09-18 19:32:18 +0200

    header setup WIP

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,21 @@ import Main from './pages/Main';
 import User from './pages/User';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    User,
-  })
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
 );
 
 export default Routes;
